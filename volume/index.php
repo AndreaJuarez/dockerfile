fb3d1b29af0e62cc5957c01a367f484f7e528e53
AndreaJuarez <1718110394@utectulancingo.edu.mx>
2021-05-30 21:37:48 +0000

    new changes

--- volume/index.php
+++ volume/index.php
@@ -1,3 +1,69 @@
-<?php
-echo "Hello World";
-?>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  </head>
+
+  <body>
+
+    <br></br>
+
+<!--Vista inicial-->
+
+  <header class="py-5 bg-image-full" style="background-image: url('https://th.bing.com/th/id/OIP.QATDD-gOKv2c2M1D4bCuQgHaHa?pid=Api&rs=1');">
+    <img class="img-fluid d-block mx-auto" src="http://utulancingo.occ.com.mx/Content/SiteManager/rue/xmx5utulanx/images/logo.png" width="500" height="1500">
+  </header>
+
+  <main role="main" class="container my-auto">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">1.2 Introducción a Docker</a>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link">Andrea Sarai Juárez Munguia</a>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+    <!-- Page Content -->
+    <b><center>¿QUÉ ES UN CONTENEDOR?</center></b>
+      <p align="justify">Un paquete de software estándar (conocido como “contenedor”) agrupa el código de una aplicación con las bibliotecas y los archivos de configuración asociados, junto con las dependencias necesarias para que la aplicación se ejecute. Esto permite a los desarrolladores y profesionales de TI implementar aplicaciones sin problemas en todos los entornos.</p>
+      <p align="justify">Una de sus principales ventajas es que requieren recursos informáticos mínimos y son rápidos y fáciles de instalar. Esto permite que se implementen en clústeres, con contenedores individuales que encapsulan componentes únicos de aplicaciones complejas. Otro punto importante a mencionar es que, separar los componentes de la aplicación en diferentes contenedores permite a los desarrolladores actualizar componentes individuales sin necesidad de rehacer la aplicación completa.</p>
+      <pre></pre>
+
+
+    <!--Cartas-->
+    <div class="row text-center">
+
+    <!--Insert-->
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-60 w-60">
+          <img class="card-img-top" src="https://th.bing.com/th/id/R5136aabef87932a42f393cd4e4ce2161?rik=jCEfvExy6uEUOQ&riu=http%3a%2f%2ftous-logos.com%2fwp-content%2fuploads%2f2018%2f04%2fLogo-Docker.png&ehk=Epuk2tXrpl9UoDQqgaER2DzN2BW3nzS%2bBiCgq2K25iw%3d&risl=&pid=ImgRaw" height="200" width="200">
+          <div class="card-body"></div>
+          <div class="card-footer">
+            <a href="insert" class="btn " style="background:#32b7ff">Docker</a>
+          </div>
+        </div>
+      </div>
+    <!--List-->
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-60 w-60">
+          <img class="card-img-top" src="https://th.bing.com/th/id/R76dfc91ab7fc328d37bbde66293c5b8b?rik=8o9TFPQFL4QfJA&riu=http%3a%2f%2fvariwiki.com%2fimages%2f1%2f1d%2fUbuntu-logo.jpg&ehk=qBb%2b4PsGyOnRjZw5NPndUBMeQe9pg%2blgWz1hOnQ0su0%3d&risl=&pid=ImgRaw" height="200" width="200">
+          <div class="card-body"></div>
+          <div class="card-footer">
+            <a href="list" class="btn " style="background:#ff8000">Ubuntu</a>
+          </div>
+        </div>
+      </div>
+
+  </body>
+
+</html>
