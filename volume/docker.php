6abf3dc3534cf3d5e1d515895da367a87acf8308
AndreaJuarez <1718110394@utectulancingo.edu.mx>
2021-05-30 22:07:34 +0000

    new changes

---
+++ volume/docker.php
@@ -0,0 +1,32 @@
+<html lang="en">
+
+<body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a href="#" class="navbar-brand">1.2 Introducción a Docker</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+    <main role="main" class="container my-auto">
+
+    <br></br>
+
+    <!--Texto de explicación-->
+      <b><center>¿QUÉ ES DOCKER?</center></b>
+      <br></br> 
+        <p align="justify">Docker es una plataforma de software que le permite crear, probar e implementar aplicaciones rápidamente. Docker empaqueta software en unidades estandarizadas llamadas contenedores que incluyen todo lo necesario para que el software se ejecute, incluidas bibliotecas, herramientas de sistema, código y tiempo de ejecución.</p>
+
+        <p align="justify">Nos proporciona una manera estándar de ejecutar nuestro código, es un sistema operativo para contenedores. De manera similar a cómo una máquina virtual virtualiza el hardware del servidor, los contenedores virtualizan el sistema operativo de un servidor. Además, Docker proporciona comandos sencillos que se pueden utilizar para crear, iniciar o detener contenedores.
+        </p>
+
+        <br></br> 
+
+        <div style="width: 500px;">
+            <a href="/"><button type="button" class="btn" style="background:#C0C0C0">Regresar</button></a>
+        </div>
+
+        <br></br>
+    
+</body>
+
+</html>
---
+++ volume/ubuntu.php
@@ -0,0 +1,31 @@
+<html lang="en">
+
+<body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a href="#" class="navbar-brand">1.2 Introducción a Docker</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+    <main role="main" class="container my-auto">
+
+    <br></br>
+
+    <!--Texto de explicación-->
+      <b><center>¿QUÉ ES UBUNTU?</center></b>
+      <br></br> 
+        <p align="justify">El sistema operativo Ubuntu es una distribución de código abierto basada en Debian, cuyo punto común es Linux, la madre de todos los softwares gratuitos que actúan como sistemas operativos en los ordenadores de medio mundo. La empresa responsable de su creación y de su mantenimiento, actualizaciones y desarrollo de nuevas versiones incluidas, es Canonical, fundada por el empresario sudafricano Mark Shuttleworth.</p>
+
+        <p align="justify">Ubuntu es un sistema operativo que es perfecto para laptops, computadoras de escritorio y servidores. Contiene todas las aplicaciones que podamos necesitar, desde procesadores de texto y aplicaciones de email, hasta software para servidor web y herramientas de programación. Es y siempre será libre de costo. No se paga por una licencia de uso, podemos descargar, usar y compartir Ubuntu.</p>
+
+        <br></br> 
+
+        <div style="width: 500px;">
+            <a href="/"><button type="button" class="btn" style="background:#C0C0C0">Regresar</button></a>
+        </div>
+
+        <br></br>
+    
+</body>
+
+</html>
