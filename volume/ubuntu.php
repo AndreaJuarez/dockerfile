aa2dbb904b0bd63d9a54bbbc871849f35b0196c7
AndreaJuarez <1718110394@utectulancingo.edu.mx>
2021-05-30 22:13:02 +0000

    last version

--- volume/ubuntu.php
+++ volume/ubuntu.php
@@ -1,5 +1,13 @@
 <html lang="en">
 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+
 <body>
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <a href="#" class="navbar-brand">1.2 Introducción a Docker</a>
